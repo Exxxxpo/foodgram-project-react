4bd9c8462d1011141eb6dca1e817cf042a9cd412
lev <ldevaev@mail.ru>
2023-05-13 13:21:35 +0500

    add filter

---
+++ backend/api/filters.py
@@ -0,0 +1,27 @@
+from django_filters.rest_framework import FilterSet, filters
+from recipes.models import Recipe, Tag
+
+
+class RecipeFilter(FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(field_name='tags__slug',
+                                             queryset=Tag.objects.all())
+    is_favorited = filters.BooleanFilter(
+        method='filter_for_favorite')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_for_shopping_cart')
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author',)
+
+    def filter_for_favorite(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return Recipe.objects.filter(favorite__user=user)
+        return Recipe.objects.all()
+
+    def filter_for_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return Recipe.objects.filter(shopping_cart__user=user)
+        return Recipe.objects.all()
