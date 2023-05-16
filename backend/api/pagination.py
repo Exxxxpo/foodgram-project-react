9254b25203e023bc9aeb7b1404005a05d2bd3496
lev <ldevaev@mail.ru>
2023-05-16 11:46:59 +0500

    fix after 1st review

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -4,4 +4,5 @@ from rest_framework.pagination import PageNumberPagination
 class PaginationForRecipe(PageNumberPagination):
     """Пагинатор для вьюсета RecipeViewSet."""
 
+    page_size = 6
     page_size_query_param = "limit"
