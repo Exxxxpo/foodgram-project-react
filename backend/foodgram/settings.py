5e6a34ba5ec17544f00f70a549d14b0dcd080e4e
lev <ldevaev@mail.ru>
2023-05-16 11:47:04 +0500

    fix after 1st review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
         "django_filters.rest_framework.DjangoFilterBackend",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 6,
 }
 
 DJOSER = {
